6644577bc5ddcf72066aa520373197eac8754760
maxwell <github email address>
2018-02-20 16:12:24 -0500

    added adapter and notes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,45 +1,38 @@
-
-import hobsWithJobs from '../data/hobsWithJobs'
+//This is just one example of an adapter class for containing our fetches
+import { RestfulAdapter } from "../adapters";
 
 export function fetchHobsWithJobs() {
-  return (dispatch) => {
-    dispatch({type: "HOBBITS_LOADING"})
-    fetch('http://localhost:3000/hobbits')
-    .then(resp=> resp.json())
-    .then(data => {
-      dispatch({type: "HOBBIT_LOAD", payload: data})
-    })
-  }
+  //using thunk, we return are returning a function here instead of
+  //a plain object.  Thunk intercepts this returned value, and if it is a
+  //function, cancels the normal event of calling our reducers, and
+  //instead, passes in 'dispatch' as an argument to the function.
+  //the fetch request was extracted out to our adapter, but still functions the same
+  return dispatch => {
+    dispatch({ type: "HOBBITS_LOADING" });
+    RestfulAdapter.indexFetch("hobbits").then(data => {
+      dispatch({ type: "HOBBIT_LOAD", payload: data });
+    });
+  };
 }
 
 export function selectHobbit(hobbit) {
   return { type: "SELECT_HOBBIT", payload: hobbit };
 }
 
-export function updateHobbit({id, name, title, key_skill}) {
+export function updateHobbit({ id, name, title, key_skill }) {
   return dispatch => {
-    fetch(`http://localhost:3000/hobbits/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-      body: JSON.stringify({hobbit: {name, title, key_skill}
-      })
-    })
-    .then(res => res.json())
-    .then(data => {
-      dispatch(setHobbit(data))
-    })
-  }
-
+    RestfulAdapter.editFetch("hobbits", id, {
+      hobbit: { name, title, key_skill }
+    }).then(data => {
+      dispatch(setHobbit(data));
+    });
+  };
 }
 
 export function setHobbit(hobbit) {
-  return {type: "SET_HOBBIT", payload: hobbit}
+  return { type: "SET_HOBBIT", payload: hobbit };
 }
 
 export function updateForm(formData) {
-
-  return {type: "UPDATE_FORM", payload: formData}
+  return { type: "UPDATE_FORM", payload: formData };
 }
---
+++ src/adapters/index.js
@@ -0,0 +1,72 @@
+//Adapters allow us to abstract out repetitive code, such as the base url and headers
+const baseUrl = `http://localhost:3000/`;
+
+function headers() {
+  return {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  };
+}
+
+export class RestfulAdapter {
+  static indexFetch(route) {
+    return fetch(`${baseUrl}/${route}`, getRequest()).then(responseHandler);
+  }
+  static showFetch(route, id) {
+    return fetch(`${baseUrl}/${route}/${id}`, getRequest()).then(
+      responseHandler
+    );
+  }
+  static createFetch(route, body) {
+    return fetch(`${baseUrl}/${route}`, postRequest(body)).then(
+      responseHandler
+    );
+  }
+  static editFetch(route, id, body) {
+    return fetch(`${baseUrl}/${route}/${id}`, patchRequest(body)).then(
+      responseHandler
+    );
+  }
+  static deleteFetch(route, id) {
+    return fetch(`${baseUrl}/${route}/${id}`, {
+      method: "DELETE",
+      headers: headers()
+    }).then(responseHandler);
+  }
+}
+
+//these may not be necessary, but if you're setting up a variety of get, post,
+//and patch requests, it may be helpful to abstract their structure as well:
+function getRequest() {
+  return {
+    headers: headers()
+  };
+}
+
+function patchRequest(body) {
+  return {
+    method: "PATCH",
+    headers: headers(),
+    body: JSON.stringify(body)
+  };
+}
+
+function postRequest(body) {
+  return {
+    method: "POST",
+    headers: headers(),
+    body: JSON.stringify(body)
+  };
+}
+
+//this is a very basic error handling function.  API responses come with a key,
+//response.ok, which will be true if the status is below 400 and false if above.
+function responseHandler(response) {
+  return response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      console.log("ERROR", response.json());
+    }
+  };
+}
