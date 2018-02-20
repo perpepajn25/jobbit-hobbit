cc452c2ecb46b961840f1f0553ae332e0bc317fc
perpepajn25 <perpepajn25@gmail.com>
2018-02-20 13:45:44 -0500

    form

---
+++ src/EditHobbit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+class EditHobbit extends React.Component {
+
+  render(){
+    return (
+
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore } from 'redux'
 import hobbitReducer from './reducers'
 
 const store = createStore(hobbitReducer)
